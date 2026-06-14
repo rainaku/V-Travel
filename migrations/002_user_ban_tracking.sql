8ed0c71ed0447b002d7b57820ede3743591c74d0
rain <129401103+rainaku@users.noreply.github.com>
2026-06-14 20:52:41 +0700

    Delete migrations directory

--- migrations/002_user_ban_tracking.sql
+++
@@ -1,9 +0,0 @@
--- Migration: Add ban tracking columns to users table
--- Tracks which admin banned the user and when
-
-ALTER TABLE users ADD COLUMN IF NOT EXISTS banned_by TEXT DEFAULT '';
-ALTER TABLE users ADD COLUMN IF NOT EXISTS banned_at TEXT DEFAULT '';
-
--- Add comment for documentation
-COMMENT ON COLUMN users.banned_by IS 'Username of admin who banned this account';
-COMMENT ON COLUMN users.banned_at IS 'ISO timestamp when the account was banned';
